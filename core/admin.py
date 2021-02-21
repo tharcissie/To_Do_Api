7bfc66c1d315f796704e0818148d1f8b8579d95b
tharcissie <tharcissieidufashe@gmail.com>
2021-02-21 10:40:09 +0200

    created the ToDo model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ToDo
 
 # Register your models here.
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ('title','description','priority')
+
+admin.site.register(ToDo, ToDoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class ToDo(models.Model):
+
+    PRIORITY = (
+        ('LOW','LOW'),
+        ('MEDIUM','MEDIUM'),
+        ('HIGH','HIGH')
+    )
+
+    title         = models.CharField(max_length=300)
+    description   = models.TextField()
+    priority      = models.CharField(max_length=10,choices=PRIORITY, default='Active')
+    created_date  = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
