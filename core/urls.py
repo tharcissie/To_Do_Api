532aeb90164077647d050e53b51cb752bef0b08b
tharcissie <tharcissieidufashe@gmail.com>
2021-02-21 15:45:49 +0200

    authenication

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from .views import *
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('todos', ToDoList.as_view()),
-    path('todos/<int:pk>',ToDoDetail.as_view()),
+    path('todos/', ToDoList.as_view()),
+    path('todos/<int:pk>/',ToDoDetail.as_view()),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
